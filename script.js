5d9aa99ff21c27f1702da9bed89b05ad3877e259
brandongon1 <48324169+brandongon1@users.noreply.github.com>
2019-03-07 09:29:58 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$("#treasureimg").click(function() {
+   $("#treasureimg").hide();
+   $("h2").text("Find the hidden image!");
+});
+$("#bronzecup").mouseenter(function(){
+    $("#bronzetrophy").show();
+    $("#bronzecup").css("width", "auto");
+});
